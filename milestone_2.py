e85c30f01d7155c418967f011ee9ad7b6adc7f02
Adam Idris <adam.idris@live.co.uk>
2022-11-15 22:51:59 +0000

    Checks that the input is a single letter

--- milestone_2.py
+++ milestone_2.py
@@ -1,11 +1,22 @@
 import random
 
-## Create a list of five favourite fruits
+# Create a list of five favourite fruits
 
 word_list = ["apple", "banana", "peach", "orange", "mango"]
 print(word_list)
 
+# Print out a random fruit from the list
+
 word = random.choice(word_list)
 print(word)
 
+# Assign the input to a variable
+
 guess = input("Please enter a single letter: ")
+
+# Check that the input is a single letter
+
+if len(guess) == 1 and guess.isalpha() == True:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
