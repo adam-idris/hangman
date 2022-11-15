0f2272720f0cd2b04261fafcd583075f4e92f977
Adam Idris <adam.idris@live.co.uk>
2022-11-15 22:15:54 +0000

    Chooses random word from list

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,4 +1,9 @@
+import random
+
 ## Create a list of five favourite fruits
 
 word_list = ["apple", "banana", "peach", "orange", "mango"]
 print(word_list)
+
+word = random.choice(word_list)
+print(word)
