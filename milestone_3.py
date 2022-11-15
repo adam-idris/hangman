ee729b2baba619607af20ccafdbffff89a87de67
Adam Idris <adam.idris@live.co.uk>
2022-11-15 23:50:29 +0000

    Functions to check guesses

--- milestone_3.py
+++ milestone_3.py
@@ -8,20 +8,28 @@ word_list = ["apple", "banana", "peach", "orange", "mango"]
 
 word = random.choice(word_list)
 
-while True:
-    guess = input("Please enter a single letter: ")
-    
-    if len(guess) == 1 and guess.isalpha() == True:
-        break
-
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-
 # Check whether the guess is in the word
-
-if guess in word:
-    print("Good guess! {} is in the word.".format(guess))  
-else:
-    print("Sorry, {} is not in the word. Try again.".format(guess))
     
+def check_guess(guess): 
+    
+    guess = guess.lower()
+    if guess in word:
+        print("Good guess! {} is in the word.".format(guess))  
+    else:
+        print("Sorry, {} is not in the word. Try again.".format(guess))
+    
+    
+def ask_for_input():
+    
+    while True:
+        guess = input("Please enter a single letter: ")
+    
+        if len(guess) == 1 and guess.isalpha() == True:
+            break
 
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+        
+    check_guess(guess)
+
+ask_for_input()
