fbcea1151d16416882b3fb9b0f725cfc3baf75b3
Idris <adam.idris@live.co.uk>
2022-11-17 14:01:44 +0000

    Solves Task 2 in Milestone 4

--- milestone_4.py
+++ milestone_4.py
@@ -1,3 +1,4 @@
+#%%
 import random
 
 class Hangman:
@@ -6,39 +7,41 @@ class Hangman:
         self.num_lives = num_lives
         
     def word(self):
-        self.word = random.choice(self.word_list)
-        
+        self.chosen_word = random.choice(self.word_list)
+        print(self.chosen_word)
+
     def word_guessed(self, letter_guessed):
-        self.word_guessed = []
-        for i in range(0, len(self.word)):
-            if self.word[i] == letter_guessed:
-                self.word_guessed[i] = letter_guessed
+        self.letter_guessed = letter_guessed
+        self.letter_list = []
+        for i in range(0, len(self.chosen_word)):
+            self.letter_list.append('_')
+            if self.chosen_word[i] == self.letter_guessed:
+                self.letter_list[i] = self.letter_guessed
             else:
-                self.word_guessed[i] = ('_')
-        
-    
+                self.letter_list[i] = ('_')
+        print(self.letter_list)
+      
     def num_letters(self):
-        pass
+
+        self.unique_letters_list = list(set(self.chosen_word))
+        self.unique_letters_left = len(self.unique_letters_list)
+
+        for i in range(self.unique_letters_left):
+            if self.unique_letters_list[i] in self.letter_list:
+                self.unique_letters_left -= 1
+        return self.unique_letters_left
     
     def word_list(self):
-        self.word_list = ["apple", "banana", "peach", "orange", "mango"]
+        self.list_used = ["apple", "banana", "peach", "orange", "mango"]
     
     def list_of_guesses(self):
-        self.list_of_guesses = []
-        
+        self.guess_list = []
+      
     def check_guess(self, guess):
+        self.guess = guess
         self.guess = guess.lower()
-        if self.guess in self.word:
-            print("Good guess! {} is in the word.".format(guess))
-            for i in range(0, len(self.word)):
-                if self.word[i] == guess:
-                    self.word_guessed[i] = guess
-            self.num_letters -= 1
-        else: 
-            self.num_lives -= 1
-            print("Sorry, {} is not in the word".format(self.guess))
-            print("You have {} lives left.".format(self.num_lives))
-        self.list_of_guesses.append(guess)
+        if self.guess in self.chosen_word:
+            print("Good guess! {} is in the word.".format(self.guess))
             
     def ask_for_input(self):
         while True:
@@ -47,8 +50,19 @@ class Hangman:
             if len(guess) != 1 and guess.isalpha() == False:
                 print("Invalid letter. Please, enter a single alphabetical character.")
             
-            elif guess in self.list_of_guesses == True:
+            elif guess in self.guess_list == True:
                 print("You already tried that letter!")
             
             else:
-                self.list_of_guesses.append(self.check_guess(guess))
+                self.guess_list.append(self.check_guess(guess))
+
+#%%
+        #     for i in range(0, len(self.chosen_word)):
+        #         if self.chosen_word[i] == guess:
+        #             self.letter_guessed[i] = guess
+        #     self.num_letters -= 1
+        # else: 
+        #     self.num_lives -= 1
+        #     print("Sorry, {} is not in the word".format(self.guess))
+        #     print("You have {} lives left.".format(self.num_lives))
+        # self.list_of_guesses.append(guess)
