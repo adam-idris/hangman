3a59fd66828841da7b3f7c73cb7f1b2dc436248f
Adam Idris <adam.idris@live.co.uk>
2022-11-18 18:28:33 +0000

    Updates check_guess to replace underscores with correctly guessed letters

--- classpractice.py
+++ classpractice.py
@@ -2,11 +2,18 @@ class Cylinder:
     def __init__(self, height, radius=1):
         self.height = height
         self.radius = radius
-        surface_area = None
-        volume = None
+        self.surface_area = self.get_surface_area()
+        self.volume = self.get_volume()
         
-    def get_surface_area(self):
-        surface_area = 
+    def get_surface_area(self):         # surface area = (2 * pi * radius * height) + (2 * pi * radius ** 2)
+        self.surface_area = (2 * 3.142 * self.radius * self.height) + (2 * 3.142 * self.radius ** 2)
+        return round(self.surface_area, 2)
         
     def get_volume(self):
-        volume = 3.142 * self.height * (self.radius ** 2)
+        self.volume = 3.142 * self.height * (self.radius ** 2)
+        return round(self.volume, 2)
+    
+cyl1 = Cylinder(21)
+
+print(cyl1.get_surface_area())
+print(cyl1.get_volume())
--- milestone_4.py
+++ milestone_4.py
@@ -1,3 +1,4 @@
+#%%
 import random
 
 class Hangman:
@@ -7,38 +8,44 @@ class Hangman:
         
     def word(self):
         self.word = random.choice(self.word_list)
-        
+        print(self.word)
+
     def word_guessed(self, letter_guessed):
-        self.word_guessed = []
+        self.letter_guessed = letter_guessed
+        self.letter_list = []
         for i in range(0, len(self.word)):
-            if self.word[i] == letter_guessed:
-                self.word_guessed[i] = letter_guessed
+            self.letter_list.append('_')
+            if self.word[i] == self.letter_guessed:
+                self.letter_list[i] = self.letter_guessed
             else:
-                self.word_guessed[i] = ('_')
-        
-    
+                self.letter_list[i] = ('_')
+        print(self.letter_list)
+      
     def num_letters(self):
-        pass
+
+        self.unique_letters_list = list(set(self.word))
+        self.unique_letters_left = len(self.unique_letters_list)
+
+        for i in range(self.unique_letters_left):
+            if self.unique_letters_list[i] in self.letter_list:
+                self.unique_letters_left -= 1
+        return self.unique_letters_left
     
     def word_list(self):
-        self.word_list = ["apple", "banana", "peach", "orange", "mango"]
+        self.list_used = ["apple", "banana", "peach", "orange", "mango"]
     
     def list_of_guesses(self):
-        self.list_of_guesses = []
-        
+        self.guess_list = []
+      
     def check_guess(self, guess):
+        self.guess = guess
         self.guess = guess.lower()
         if self.guess in self.word:
-            print("Good guess! {} is in the word.".format(guess))
-            for i in range(0, len(self.word)):
-                if self.word[i] == guess:
-                    self.word_guessed[i] = guess
+            print("Good guess! {} is in the word.".format(self.guess))
+            for i in range(len(self.word)):
+                if self.guess == self.word[i]:
+                    self.letter_list[i] = self.guess
             self.num_letters -= 1
-        else: 
-            self.num_lives -= 1
-            print("Sorry, {} is not in the word".format(self.guess))
-            print("You have {} lives left.".format(self.num_lives))
-        self.list_of_guesses.append(guess)
             
     def ask_for_input(self):
         while True:
@@ -47,8 +54,19 @@ class Hangman:
             if len(guess) != 1 and guess.isalpha() == False:
                 print("Invalid letter. Please, enter a single alphabetical character.")
             
-            elif guess in self.list_of_guesses == True:
+            elif guess in self.guess_list == True:
                 print("You already tried that letter!")
             
             else:
-                self.list_of_guesses.append(self.check_guess(guess))
+                self.guess_list.append(self.check_guess(guess))
+
+#%%
+        #     for i in range(0, len(self.word)):
+        #         if self.word[i] == guess:
+        #             self.letter_guessed[i] = guess
+        #     self.num_letters -= 1
+        # else: 
+        #     self.num_lives -= 1
+        #     print("Sorry, {} is not in the word".format(self.guess))
+        #     print("You have {} lives left.".format(self.num_lives))
+        # self.list_of_guesses.append(guess)
