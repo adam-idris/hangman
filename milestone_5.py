39a52095330d41d6801e41afa3d8f0564b380d27
Idris <adam.idris@live.co.uk>
2023-02-13 17:00:45 +0000

    Includes hangman pictures

--- milestone_5.py
+++ milestone_5.py
@@ -11,8 +11,34 @@ class Hangman:
         self.num_letters = len(list(set(self.word)))
         self.num_lives = num_lives
         self.list_of_guesses = []
+        self.man = ''
         
-      
+    def hangman_picture(self, num_lives):
+
+        if num_lives == 5:
+            self.man = ''
+            return self.man
+
+        if num_lives == 4:
+            self.man = '  +---+\n      |\n      |\n      |\n      |\n      |\n======='
+            return self.man
+
+        elif num_lives == 3:
+            self.man = '  +---+\n      |\n  O   |\n      |\n      |\n      |\n======='
+            return self.man
+
+        elif num_lives == 2:
+            self.man = '  +---+\n      |\n  O   |\n  |   |\n / \  |\n      |\n======='
+            return self.man
+
+        elif num_lives == 1:
+            self.man = '  +---+\n      |\n  O   |\n /|\  |\n / \  |\n      |\n======='
+            return self.man
+            
+        elif num_lives == 0:
+            self.man = '  +---+\n  |   |\n  O   |\n /|\  |\n / \  |\n      |\n======='
+            return self.man
+
     def check_guess(self, guess):
         self.guess = guess
         self.guess = self.guess.lower()
@@ -22,11 +48,22 @@ class Hangman:
                 if self.word[i] == self.guess:
                     self.word_guessed[i] = self.guess
             self.num_letters -= 1
+            print("\n {}".format(self.word_guessed))
+            print("\n")
+            print(self.hangman_picture(self.num_lives))
+
         else:
             self.num_lives -= 1
             print("Sorry {} is not in the word.".format(self.guess))
-            print("You have {} lives left".format(self.num_lives))
-        self.list_of_guesses.append(self.guess)       
+            print("\n {}".format(self.word_guessed))
+            print("\nYou have {} lives left".format(self.num_lives))
+            print("\n")
+            print(self.hangman_picture(self.num_lives))
+            
+
+        self.list_of_guesses.append(self.guess)
+        print("Guessed letters: " + ", ".join(list(set(self.list_of_guesses))) + "\n")
+
             
     def ask_for_input(self):
         
@@ -51,7 +88,7 @@ def play_game(word_list):
         if game.num_lives == 0:
             print("You lost!")
             break
-        elif game.num_lives > 0:
+        elif game.num_letters > 0:
             game.ask_for_input()
         else:
             print("Congratulations. You won the game!")
